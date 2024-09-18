0a8c9cd2758b854fe29ba7a5034d9daa6007743e
mile <darimile@gmail.com>
2024-09-18 21:39:16 +0200

    add generarId

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   import Presupuesto from './components/Presupuesto.vue';
   import ControlPresupuesto from './components/ControlPresupuesto.vue';
   import Modal from './components/Modal.vue';
+  import {generarId} from './helpers'; 
   import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
 
   const modal = reactive({
@@ -50,7 +51,8 @@
   const guardarGasto = () => {
     gastos.value.push({
       ...gasto, 
-      id: 123,
+      id: generarId(),
+
       
     })
   }
