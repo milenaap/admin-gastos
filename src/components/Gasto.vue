09b4a1fb264225303a22e213ba765696483a0b42
mile <darimile@gmail.com>
2024-09-20 22:20:25 +0200

    add icons

--- src/components/Gasto.vue
+++ src/components/Gasto.vue
@@ -1,13 +1,53 @@
 <template>
-    <div>
-        <p>Gasto</p>
+    <div class="gasto sombra">
+        <div class="contenido">
+            <img :src="diccionarioIconos[gasto.categoria]" alt="Icono gasto" class="icono">
+            <div class="detalles">
+                <p class="categoria">{{ gasto.categoria }}</p>
+            </div>
+
+        </div>
+
+        <p class="cantidad">{{ formatearCantidad(gasto.cantidad) }}</p>
+        <p class="nombre">{{ gasto.nombre }}</p>
+        <p class="fecha">{{ formatearFecha(gasto.fecha) }}</p>
+
     </div>
+
+
 </template>
 
 <script setup>
 
+import { formatearCantidad, formatearFecha } from '../helpers/index';
+import IconoAhorro from '../assets/img/icono_ahorro.svg'
+import IconoCasa from '../assets/img/icono_casa.svg'
+import IconoComida from '../assets/img/icono_comida.svg'
+import IconoGastos from '../assets/img/icono_gastos.svg'
+import IconoOcio from '../assets/img/icono_ocio.svg'
+import IconoSalud from '../assets/img/icono_salud.svg'
+import IconoSuscripciones from '../assets/img/icono_suscripciones.svg'
+
+
+const diccionarioIconos = {
+    ahorro: IconoAhorro,
+    comida: IconoComida,
+    casa: IconoCasa,
+    gastos: IconoGastos,
+    ocio: IconoOcio,
+    salud: IconoSalud,
+    suscripciones: IconoSuscripciones
+}
+
+const props = defineProps({
+    gasto: {
+        type: Object,
+        required: true
+    }
+})
+
+
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
